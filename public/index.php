0c045627f4088949e0d0c99903e41586ee4848fb
aelita <ai2work@yandex.ru>
2023-04-26 17:18:50 +0500

    welcome slim

---
+++ public/index.php
@@ -0,0 +1,23 @@
+<?php
+
+
+// Подключение автозагрузки через composer
+require __DIR__.'/../vendor/autoload.php';
+
+use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+$app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function ($request, $response) {
+    $response->getBody()->write('Welcome to Slim!');
+
+    return $response;
+    // Благодаря пакету slim/http этот же код можно записать короче
+    // return $response->write('Welcome to Slim!');
+});
+$app->get('/users', function ($request, $response) {
+    return $response->write('GET /users');
+});
+
+$app->run();
